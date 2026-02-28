19090b1157285e92f792737903ffa065f95b2b35
FrogOnABike <mark.skrzypczyk.com>
2026-02-28 22:42:50 +0000

    Ch1L5 Done

--- main.go
+++ main.go
@@ -11,12 +11,44 @@ import (
 
 const inputFilePath = "messages.txt"
 
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	lines := make(chan string)
+	go func() {
+		defer close(lines)
+		defer f.Close()
+		var line string
+		for {
+			b := make([]byte, 8)
+			n, err := f.Read(b)
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					if line != "" {
+						lines <- line
+					}
+					break
+				}
+				fmt.Printf("error: %s\n", err.Error())
+				break
+			}
+			parts := strings.Split(string(b[:n]), "\n")
+			for i, part := range parts {
+				if i == 0 {
+					line += part
+				} else {
+					lines <- line
+					line = part
+				}
+			}
+		}
+	}()
+	return lines
+}
+
 func main() {
 	f, err := os.Open(inputFilePath)
 	if err != nil {
 		log.Fatalf("could not open %s: %s\n", inputFilePath, err)
 	}
-	defer f.Close()
 
 	fmt.Printf("Reading data from %s\n", inputFilePath)
 	fmt.Println("=====================================")
@@ -24,43 +56,8 @@ func main() {
 	//  Each new line should start with "read:"
 	// If the last line does not end with a newline character, print it as well.
 
-	var line string
-	for {
-		b := make([]byte, 8)
-		n, err := f.Read(b)
-		if err != nil {
-			if errors.Is(err, io.EOF) {
-				if line != "" {
-					fmt.Printf("read: %s\n", line)
-				}
-				break
-			}
-			fmt.Printf("error: %s\n", err.Error())
-			break
-		}
-		parts := strings.Split(string(b[:n]), "\n")
-		for i, part := range parts {
-			if i == 0 {
-				line += part
-			} else {
-				fmt.Printf("read: %s\n", line)
-				line = part
-			}
-		}
+	lines := getLinesChannel(f)
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
 	}
 }
-
-// L3 code below: read 8 bytes at a time and print the string representation of the bytes read
-// for {
-// 	b := make([]byte, 8, 8)
-// 	n, err := f.Read(b)
-// 	if err != nil {
-// 		if errors.Is(err, io.EOF) {
-// 			break
-// 		}
-// 		fmt.Printf("error: %s\n", err.Error())
-// 		break
-// 	}
-// 	str := string(b[:n])
-// 	fmt.Printf("read: %s\n", str)
-// }
