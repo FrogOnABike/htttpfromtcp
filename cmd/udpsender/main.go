5cf83ac0914886e8cd8de61ac664ba77c768f4ca
FrogOnABike <mark.skrzypczyk.com>
2026-03-11 22:33:26 +0000

    Ch4L1 done

--- cmd/udpsender/main.go
+++ cmd/udpsender/main.go
@@ -1,14 +1,37 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
 func main() {
 	addr, err := net.ResolveUDPAddr("udp4", "localhost:42069")
 	if err != nil {
 		log.Fatalf("could not resolve address: %s\n", err)
 	}
-	
+
 	conn, err := net.DialUDP("udp4", nil, addr)
 	if err != nil {
 		log.Fatalf("could not dial: %s\n", err)
 	}
 	defer conn.Close()
-	
+
+	input := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print(">")
+		text, err := input.ReadString('\n')
+		if err != nil {
+			log.Printf("could not read input: %s\n", err)
+			continue
+		}
+
+		_, err = conn.Write([]byte(text))
+		if err != nil {
+			log.Printf("could not send message: %s\n", err)
+		}
+	}
+}
